b28472bcff911d1e0b4d9339e3a6db8fada73e8d
Luigi <71444160+LuigiS91@users.noreply.github.com>
2020-10-30 23:17:49 +0100

    Aggiornamento : 30/10/2020 23:17:49

--- commit.py
+++ commit.py
@@ -7,5 +7,6 @@ import os
 now = datetime.now()
 commit_name = '"Aggiornamento : '+now.strftime("%d/%m/%Y %H:%M:%S"+'"')
 
+os.system("git add *")
 os.system("git commit -am "+ commit_name)
 os.system("git push")
